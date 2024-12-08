94a012c5ad6d3cfb3f5b2b426703e3cdcc974e3b
tatoei <159608764+tatoei@users.noreply.github.com>
2024-12-08 23:36:38 +0700

    chore

--- dist/upload/upload.module.js
+++ dist/upload/upload.module.js
@@ -11,6 +11,7 @@ const common_1 = require("@nestjs/common");
 const platform_express_1 = require("@nestjs/platform-express");
 const upload_controller_1 = require("./upload.controller");
 const upload_service_1 = require("./upload.service");
+const mongoose_1 = require("@nestjs/mongoose");
 let UploadModule = class UploadModule {
 };
 exports.UploadModule = UploadModule;
@@ -20,6 +21,7 @@ exports.UploadModule = UploadModule = __decorate([
             platform_express_1.MulterModule.register({
                 dest: './uploads',
             }),
+            mongoose_1.MongooseModule.forRoot('mongodb+srv://toeiisararawee:toeiisararawee@cluster0.jodvh.mongodb.net/'),
         ],
         controllers: [upload_controller_1.UploadController],
         providers: [upload_service_1.UploadService],
--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { MulterModule } from '@nestjs/platform-express';
 import { UploadController } from './upload.controller';
 import { UploadService } from './upload.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MulterModule.register({
       dest: './uploads', // กำหนดตำแหน่งการเก็บไฟล์
     }),
+    MongooseModule.forRoot('mongodb+srv://toeiisararawee:toeiisararawee@cluster0.jodvh.mongodb.net/'),
   ],
   controllers: [UploadController],
   providers: [UploadService],
